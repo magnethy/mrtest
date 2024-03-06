55fd1bc6f7b7b1cac9f95cef3d5e0b75d21e0bb3
Magne Thyrhaug <magne.thyrhaug@cgi.com>
2024-03-06 21:48:05 +0100

    Cleaned up JSONHttpClient; reusing more code

--- pkg/http_client/json_http_client.go
+++ pkg/http_client/json_http_client.go
@@ -12,58 +12,27 @@ type JSONHttpClient struct{}
 
 // Get fetches a URL and fills the `v` parameter with the JSON response
 func (h *JSONHttpClient) Get(url string, headers map[string]string, v any) error {
-	req, err := http.NewRequest("GET", url, nil)
+	err := doRequest("GET", url, headers, nil, v)
 	if err != nil {
-		return fmt.Errorf("could not create request, %s", err)
-	}
-
-	for headerKey, headerValue := range headers {
-		req.Header.Add(headerKey, headerValue)
-	}
-
-	client := http.Client{}
-	response, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("could not get response, %s", err)
-	}
-
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			fmt.Printf("could not close response body: %s", err)
-		}
-	}(response.Body)
-
-	jsonBody, err := io.ReadAll(response.Body)
-	if err != nil {
-		return fmt.Errorf("error read response body, %s", err)
-	}
-
-	err = json.Unmarshal(jsonBody, &v)
-
-	if err != nil {
-		return fmt.Errorf("could not convert response body to json, %s", err)
+		return fmt.Errorf("could not do POST request: %s", err)
 	}
 
 	return nil
 }
 
 func (h *JSONHttpClient) Post(url string, headers map[string]string, body any, v any) error {
-	jsonRequestBody, _ := json.Marshal(body)
-	requestbodyBuffer := bytes.NewBuffer(jsonRequestBody)
-	req, err := http.NewRequest("POST", url, requestbodyBuffer)
+	err := doRequest("POST", url, headers, body, v)
 	if err != nil {
-		return fmt.Errorf("could not create POST request, %s", err)
+		return fmt.Errorf("could not do POST request: %s", err)
 	}
 
-	for headerKey, headerValue := range headers {
-		req.Header.Add(headerKey, headerValue)
-	}
+	return nil
+}
 
-	client := http.Client{}
-	response, err := client.Do(req)
+func doRequest(verb string, url string, headers map[string]string, body any, v any) error {
+	response, err := submitRequest(verb, url, headers, body, v)
 	if err != nil {
-		return fmt.Errorf("could not get response, %s", err)
+		return fmt.Errorf("could not get %s response, %s", verb, err)
 	}
 
 	defer func(Body io.ReadCloser) {
@@ -73,13 +42,43 @@ func (h *JSONHttpClient) Post(url string, headers map[string]string, body any, v
 		}
 	}(response.Body)
 
+	err = getResponseBody(response, &v)
+	if err != nil {
+		return fmt.Errorf("could not get response body from %s request: %s", verb, err)
+	}
+
+	return nil
+}
+
+func submitRequest(verb string, url string, headers map[string]string, body any, v any) (*http.Response, error) {
+	jsonRequestBody, _ := json.Marshal(body)
+	requestbodyBuffer := bytes.NewBuffer(jsonRequestBody)
+
+	req, err := http.NewRequest(verb, url, requestbodyBuffer)
+	if err != nil {
+		return &http.Response{}, fmt.Errorf("could not create request, %s", err)
+	}
+
+	for headerKey, headerValue := range headers {
+		req.Header.Add(headerKey, headerValue)
+	}
+
+	client := http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return &http.Response{}, fmt.Errorf("could not send request: %s", err)
+	}
+
+	return response, nil
+}
+
+func getResponseBody(response *http.Response, v any) error {
 	jsonResponseBody, err := io.ReadAll(response.Body)
 	if err != nil {
 		return fmt.Errorf("error read response body, %s", err)
 	}
 
 	err = json.Unmarshal(jsonResponseBody, &v)
-
 	if err != nil {
 		return fmt.Errorf("could not convert response body to json, %s", err)
 	}
